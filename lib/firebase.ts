15b4b300f1718037bc2c5283b8aba2ef0095e92b
kurosan5 <kurowassan55555@gmail.com>
2024-10-27 13:32:08 +0900

    firebaseプロジェクト接続

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHGnc9bVpCL498IRYsY9iQTjbU-uX0kW0",
+  authDomain: "react-to-do-6082b.firebaseapp.com",
+  projectId: "react-to-do-6082b",
+  storageBucket: "react-to-do-6082b.appspot.com",
+  messagingSenderId: "178024563489",
+  appId: "1:178024563489:web:6b409e5d36885561bdf460"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
